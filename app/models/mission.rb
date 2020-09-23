6164d6cac0eecb39b2d22b23900fd4d1f3a5970f
KevJSDevelopment <Kshhn12@gmail.com>
2020-09-22 20:52:22 -0400

    done

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,5 +1,5 @@
 class Mission < ApplicationRecord
-    validates :name, presence: true
+    validates :name, presence: true, uniqueness: {case_sensitive: false}
     validates :planet_id, presence: true
     validates :scientist_id, presence: true
 
